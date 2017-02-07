0e052dc34063e431b9b3eb9790251bc96b02e8ff
QuadFlask <pop9310@gmail.com>
2017-02-07 23:01:35 +0900

    4.4

--- book2/ch4.py
+++ book2/ch4.py
@@ -30,3 +30,114 @@ from dataset.mnist import load_mnist
 
 print(x_train.shape)
 print(t_train.shape)
+
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
+
+print(np.random.choice(60000, 10))
+
+
+def cross_entropy_error(y, t):  # if one-hot encoding
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y)) / batch_size
+
+
+def cross_entropy_error2(y, t):  # if not one-hot encoding
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arrange(batch_size), t])) / batch_size
+
+
+def numerical_diff_bad(f, x):
+    h = 1e-10
+    return (f(x + h) - f(x)) / h
+
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+
+def function_2(x):
+    return x[0] ** 2 + x[1] ** 2
+
+
+init_x = np.array([-3., 4.])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
+
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+
+print("simple net")
+net = simpleNet()
+print(net.W)
+x = np.array([0.6, 0.9])
+p = net.predict(x)
+print(p)
+
+print(np.argmax(p))
+
+t = np.array([0, 0, 1])
+print(net.loss(x, t))
+
+def f(W):
+    return net.loss(x ,t)
+
+dW = numerical_gradient(f, net.W)
+print(dW)
+
